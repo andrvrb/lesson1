c0037f4e66ac9caa2ab653bc58ee75c55737117f
andrvrb <andr-pnz@mail.ru>
2022-03-27 14:20:08 +0300

    first commit

---
+++ src/main/java/com/egartech/lesson1/MainApp.java
@@ -0,0 +1,10 @@
+package com.egartech.lesson1;
+
+import com.egartech.lesson1.animals.Cat;
+
+public class MainApp {
+    public static void main(String[] args) {
+    Cat cat = new Cat("Barsik", "White", 2);
+        System.out.println("dfsdf");
+    }
+}
